5b808b1510742ee7657a549597fa54618a9b3cff
Marcos Soares <mavmaso@gmail.com>
2021-09-04 14:58:54 -0300

    chore: refactor router.ex

--- lib/tri_web/router.ex
+++ lib/tri_web/router.ex
@@ -5,27 +5,17 @@ defmodule TriWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", TriWeb do
+  scope "/", TriWeb do
     pipe_through :api
 
-    resources "/users", UserController, only: [:show, :create]
+    post "/user", UserController, :create
 
     post "/login", SessionController, :login
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  scope "/", TriWeb do
+    pipe_through :api
 
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: TriWeb.Telemetry
-    end
+    get "/user/:id", UserController, :show
   end
 end
